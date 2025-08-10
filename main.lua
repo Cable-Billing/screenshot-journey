bcfe314964c82a173598df4eb226db07e9dd384c
Caleb Billing <billing.caleb14@gmail.com>
2025-08-10 17:20:22 +0930

    Created Helper Function

--- main.lua
+++ main.lua
@@ -10,17 +10,21 @@ ScreenshotJourney_Config = ScreenshotJourney_Config or {
 local f = CreateFrame("Frame")
 local elapsedSinceLast = 0
 
+local function TakeScreenshot()
+    Screenshot()
+end
+
 f:SetScript("OnEvent", function(self, event, ...)
     if event == "PLAYER_LEVEL_UP" and ScreenshotJourney_Config.levelUp then
-        Screenshot()
+        TakeScreenshot()
     elseif event == "QUEST_TURNED_IN" and ScreenshotJourney_Config.questComplete then
-        Screenshot()
+        TakeScreenshot()
     elseif event == "BOSS_KILL" and ScreenshotJourney_Config.bossKill then
-        Screenshot()
+        TakeScreenshot()
     elseif event == "COMBAT_LOG_EVENT_UNFILTERED" and ScreenshotJourney_Config.pvpKill then
         local _, subEvent, _, _, _, _, _, destGUID, destName, destFlags = CombatLogGetCurrentEventInfo()
         if subEvent == "PARTY_KILL" and bit.band(destFlags, COMBATLOG_OBJECT_TYPE_PLAYER) > 0 then
-            Screenshot()
+            TakeScreenshot()
         end
     end
 end)
@@ -35,7 +39,7 @@ f:SetScript("OnUpdate", function(self, elapsed)
         elapsedSinceLast = elapsedSinceLast + elapsed
         if elapsedSinceLast >= ScreenshotJourney_Config.periodicInterval then
             elapsedSinceLast = 0
-            Screenshot()
+            TakeScreenshot()
         end
     end
 end)
